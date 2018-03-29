8e591f4b758ee88ef2aadd7d69b39880d456f0f8
Alaa <alaa0selim@gmail.com>
2018-03-29 20:49:01 +0200

    test 123

---
+++ index.js
@@ -0,0 +1,15 @@
+var app = require("express")();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+app.get('/', (req, res)=>{
+   res.send('welcome!');
+})
+
+
+
+
+http.listen(9000,null,()=>{
+  console.log('listening on port 9000');
+});
