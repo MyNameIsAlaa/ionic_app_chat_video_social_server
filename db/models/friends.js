34e713df01de90481257251bc9e8a83bc19eb8b8
Alaa <alaa0selim@gmail.com>
2018-03-31 01:34:57 +0200

    fix get my friends again

--- db/models/friends.js
+++ db/models/friends.js
@@ -2,7 +2,7 @@ var Mongoose = require("mongoose");
 
 var Friend = new Mongoose.Schema({
     Owner: {type: Mongoose.Schema.Types.ObjectId, ref:'User'},
-    FriendID: {type: Mongoose.Schema.Types.ObjectId, ref:'User'},
+    Friend: {type: Mongoose.Schema.Types.ObjectId, ref:'User'},
 });
 
 module.exports = Mongoose.model("Friend", Friend);
