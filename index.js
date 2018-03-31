488affc7ca8541984b8fafa204ef3069950087f9
Alaa <alaa0selim@gmail.com>
2018-03-31 07:42:55 +0200

    add socket io

--- index.js
+++ index.js
@@ -82,6 +82,7 @@ io.on('connection', function (socket) {
                         });
                  }else{
                      //user not online save to offline message
+                     console.log("reciever" + data.to + "is not online!");
                  }
            })
    
