84259bf32e3164a7affbd31d99d23aae6198b191
Alaa <alaa0selim@gmail.com>
2018-04-01 09:19:54 +0200

    fix friends

--- routes/friends.js
+++ routes/friends.js
@@ -15,7 +15,13 @@ Mongoose.connect(config.mlab.URL,(error)=>{
 Router.get('/', passport.authenticate('jwt', {session: false}), (req,res)=>{
     Friend.find({Owner:Mongoose.Types.ObjectId(req.user._id)}).populate("Friend", "-password").exec((error, friends)=>{
         if(error) return res.status(500).json({"error": error});
-        if(friends) return res.status(200).json(friends);
+        let data;
+        if(friends){
+             friends.forEach(function(friend) {
+               data[friend.Friend._id] = friend.Friend;
+              });
+            return res.status(200).json(data);
+        } 
     })
 });
 
