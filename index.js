9419e8389fe7e2ce7a4f7552481762bc1f3102c7
Alaa <alaa0selim@gmail.com>
2018-04-04 11:52:17 +0200

    save messages in db for offline users

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ io.on('connection', function (socket) {
             Message.find({to: Mongose.Types.ObjectId(data.id) }, (err, messages)=>{
                 messages.forEach((msg)=>{
                   socket.to(socket.id).emit('incoming_message',{from: msg.from,message: msg.message, username: msg.username});
-                  console.log({socket: socket.id, userid: data.i});
+                  console.log({socket: socket.id, userid: data.id});
                   Message.findByIdAndRemove(msg._id).exec();
               });
           });
